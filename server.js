a5de7d87aa2d28747f1c07ac289bd6de1da5c246
Caroline <gekniateh@gmail.com>
2024-05-22 10:49:58 +0800

    config cookie-pars & body-parser

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const ErrorHandler = require('./utilities/ErrorHandler');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 require('dotenv').config();
 require('./config/database')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true, limit:"50mb"}));
+app.use(fileUpload({useTempFiles: true}));
+app.use(ErrorHandler);
 
 
 const PORT = 4000;
