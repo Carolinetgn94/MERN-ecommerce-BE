847e9009a21ababd85e756c2fc9d5e03dfbb86ea
Carolinetgn94 <gekniateh@gmail.com>
2024-06-17 17:21:56 +0800

    edit errorhandler

--- utilities/ErrorHandler.js
+++ utilities/ErrorHandler.js
@@ -1,12 +1,26 @@
-class ErrorHandler extends Error{
-    constructor(message,statusCode){
-        super(message);
-        this.statusCode = statusCode
-        console.log('ErrorHandler constructor called with:', message, statusCode);
+// class ErrorHandler extends Error{
+//     constructor(message,statusCode){
+//         super(message);
+//         this.statusCode = statusCode
+//         console.log('ErrorHandler constructor called with:', message, statusCode);
 
-        Error.captureStackTrace(this,this.constructor);
+//         Error.captureStackTrace(this,this.constructor);
 
-    }
+//     }
     
-}
+// }
+// utilities/ErrorHandler.js
+const ErrorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+  
+    res.status(statusCode).json({
+      success: false,
+      message,
+    });
+  };
+  
+  module.exports = ErrorHandler;
+  
+
 module.exports = ErrorHandler
